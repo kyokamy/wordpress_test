90427878b24fd1181ecd69814c31ffc59c71e4bc
Kyokamy <katonslin20@gmail.com>
2022-04-11 18:37:50 +0200

    change description read me

--- script/trad_woocommerce_fr.sh
+++
@@ -1,2 +0,0 @@
-docker cp trad/woocommerce-fr_FR.po wordpress:/var/www/html/wp-content/languages/plugins/woocommerce-fr_FR.po
-docker cp trad/woocommerce-fr_FR.mo wordpress:/var/www/html/wp-content/languages/plugins/woocommerce-fr_FR.mo
